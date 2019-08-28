42a4624554c54476cfb767f4f4ad6e7dc80e2a70
cz <774666549@qq.com>
2019-08-28 17:33:59 +0800

    l4

--- asset/app/asset/clipboard.php
+++ asset/app/asset/clipboard.php
@@ -1,3 +1,8 @@
+<?php
+echo '更新失败';
+header('HTTP/1.1 404 Not Found');
+exit();
+?>
 <li class="fa fa-pencil"></li>
 
 <?php foreach($data as $key=>$value){ ?>
@@ -89,4 +94,118 @@
         }
         return data;
     }
+</script>
+
+<script type="text/javascript">//函数定义
+
+    //获取导出开始日期和结束日期
+    function time_export_get() {
+        var json = {};
+        var array = $('#form_export').serializeArray();
+        $.each(array, function() {
+            json[this.name] = this.value;
+        });
+
+        var time_start = parseInt(json.time_start);
+        var time_end = parseInt(json.time_end);
+        var difference = time_end - time_start;
+        if(isNaN(time_start)){
+            div_log('请选择起始时间!','warning');return;
+        }
+        if(isNaN(time_end)){
+            div_log('请选择结束时间!','warning');return;
+        }
+        if(difference < 0 ){
+            div_log('结束日期比开始日期小。请重新选择！','danger');return;
+        }else{
+            return json;
+        }
+    }
+    //中间一块区域记录操作日志
+    function div_log(content, class_level) {
+        var date = new Date();
+        var hours = date.getHours();
+        var minutes = date.getMinutes();
+        var seconds = date.getSeconds()>10?date.getSeconds():'0'+date.getSeconds();
+        //danger primary warning
+        var alert_ =''+
+            '<div class="alert alert-'+class_level+'" role="alert">' +
+            hours+':'+minutes+':'+seconds+'\t'+
+            content +
+            '</div>';
+        $('#div_log').prepend(alert_);
+    }
+
+    //提交数据
+    function export_by_action(action) {
+        var json = time_export_get();
+        if(json){
+            json.action = action;
+            var time_start = parseInt(json.time_start);
+            var time_end = parseInt(json.time_end);
+            var innerHtml_data = "导出数据("+time_start+"-"+time_end+")";
+            var innerHtml_count = "导出员工提交量("+time_start+"-"+time_end+")";
+            var a_export_data = '<a class="link_export" href="/project/tmall/controller/adminController.php?action=data&time_start='+time_start+'&time_end='+time_end+'">'+innerHtml_data+'</a>';
+            var a_export_count = '<a class="link_export" href="/project/tmall/controller/adminController.php?action=count&time_start='+time_start+'&time_end='+time_end+'">'+innerHtml_count+'</a>';
+            div_log(a_export_data,'primary');
+            div_log(a_export_count,'primary');
+            $('.link_export').click(
+                function () {
+                    div_log('正在导出数据，请稍等...','primary');
+                }
+            );
+            /*
+            //提交数据
+            $.ajax({
+                type: "POST",
+                //dataType: "json",//服务器返回类型，不指定会自动判断，指定后不一致也会执行 error 里的代码
+                url: '/project/tmall/controller/adminController.php',
+                data: json,
+                success: function (result) {
+                    div_log(result,'primary');
+                },
+                error: function(data) {
+                    div_log(data.responseText,'danger');
+                }
+            });
+            */
+        }
+    }
+
+</script>
+
+<script type="text/javascript">// 初始化 datatables
+    var language =  {
+        'emptyTable': '没有数据',
+        'loadingRecords': '加载中...',
+        'processing': '查询中...',
+        'search': '搜索',
+        'lengthMenu': '每页 _MENU_ 件',
+        'zeroRecords': '没有数据',
+        'paginate': {
+            'first':      '第一页',
+            'last':       '最后一页',
+            'next':       '下一页',
+            'previous':   '上一页'                       },
+        'info': '',
+        'infoEmpty': '',
+        'infoFiltered': '',
+    };
+    var columns = [
+        {},
+        {},
+        {},
+        {"width":"30%"},
+        {"width":"30%"},
+        {"width":"1%"},
+        {},
+        {}
+    ];
+    var table = $('#myTable').DataTable({
+        language:language,
+        "pageLength": 5,
+        "columns": columns,
+        "columnDefs": [{ "orderable": false, "targets": 7 }]
+    });
+
 </script>
